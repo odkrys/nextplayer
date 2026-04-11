f7088b96e2b6e8e32eba1f4a68ae1f82d1098303
odkrys <naulisreal@gmail.com>
2026-04-11 11:10:02 +0900

    player settings: change the default value

--- core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/PlayerPreferences.kt
+++ core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/PlayerPreferences.kt
@@ -5,15 +5,15 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PlayerPreferences(
     val resume: Resume = Resume.YES,
-    val rememberPlayerBrightness: Boolean = false,
+    val rememberPlayerBrightness: Boolean = true,
     val playerBrightness: Float = 0.5f,
     val minDurationForFastSeek: Long = 120000L,
     val rememberSelections: Boolean = true,
-    val playerScreenOrientation: ScreenOrientation = ScreenOrientation.VIDEO_ORIENTATION,
+    val playerScreenOrientation: ScreenOrientation = ScreenOrientation.AUTOMATIC,
     val playerVideoZoom: VideoContentScale = VideoContentScale.BEST_FIT,
     val defaultPlaybackSpeed: Float = 1.0f,
     val autoplay: Boolean = true,
-    val autoPip: Boolean = true,
+    val autoPip: Boolean = false,
     val autoBackgroundPlay: Boolean = false,
     val loopMode: LoopMode = LoopMode.OFF,
     val shuffleMode: Boolean = false,
@@ -25,8 +25,8 @@ data class PlayerPreferences(
     val enableBrightnessSwipeGesture: Boolean = true,
     val useSeekControls: Boolean = true,
     val useZoomControls: Boolean = true,
-    val enablePanGesture: Boolean = false,
-    val doubleTapGesture: DoubleTapGesture = DoubleTapGesture.BOTH,
+    val enablePanGesture: Boolean = true,
+    val doubleTapGesture: DoubleTapGesture = DoubleTapGesture.FAST_FORWARD_AND_REWIND,
     //val useLongPressControls: Boolean = false,
     val longPressGesture: LongPressGesture = LongPressGesture.PLAYBACK_SPEED,
     val longPressControlsSpeed: Float = 2.0f,
@@ -65,11 +65,11 @@ data class PlayerPreferences(
 ) {
 
     companion object {
-        const val DEFAULT_SEEK_INCREMENT = 10
+        const val DEFAULT_SEEK_INCREMENT = 5
         const val DEFAULT_SEEK_SENSITIVITY = 0.50f
-        const val DEFAULT_VOLUME_GESTURE_SENSITIVITY = 0.50f
+        const val DEFAULT_VOLUME_GESTURE_SENSITIVITY = 0.70f
         const val DEFAULT_BRIGHTNESS_GESTURE_SENSITIVITY = 0.50f
         const val DEFAULT_SUBTITLE_TEXT_SIZE = 20
-        const val DEFAULT_CONTROLLER_AUTO_HIDE_TIMEOUT = 4
+        const val DEFAULT_CONTROLLER_AUTO_HIDE_TIMEOUT = 3
     }
 }
